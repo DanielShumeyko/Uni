0e07288ed49f954c62e1deb3fb1dbbb5471e841d
Daniel Shumeyko <shumeykodanil2@gmail.com>
2019-09-29 17:21:27 +0300

    For now only blank templates

---
+++ NumMethods/Lab1/nonlinear.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# This function is basically the function f(x) on the left side of the equation. Here 'nlf' stands for nonlinear function.
+def nlf(x):
+    return x**4 + np.sin(x) - 12*x + 1
+
+class Modified_Newton:
+    pass
+class Dichotomy:
+    pass
