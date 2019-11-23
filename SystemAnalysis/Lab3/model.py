80120500f30aeb9a2a44eca54bda323ff1821b2d
Daniel Shumeyko <shumeykodanil2@gmail.com>
2019-11-23 16:21:25 +0200

    Lab 3 done and working

--- SystemAnalysis/Lab3/model.py
+++ SystemAnalysis/Lab3/model.py
@@ -4,7 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-x_final = np.array([2, 0, 0])
+x_final = np.matrix([
+    [2],
+    [0], 
+    [0]
+     ], dtype=float)
 class DynamicModel:
     def __init__(self, a1, a2, b, q, t, ko, xo):
         self.changeData(a1, a2, b, q, t, ko, xo)
@@ -17,12 +21,12 @@ class DynamicModel:
         self.q = q
         self.to = t
         self.ko = ko
-        self.xo = np.array([xo, xo, xo])
-        self.t_range = np.arange(0, 10 + self.to, self.to)
+        self.xo = np.matrix([[xo], [xo], [xo]])
+        self.t_range = np.arange(0, 1000 + self.to, self.to)
         self.t_range = self.t_range[:self.ko]
         self.A = np.matrix([[0, 1, 0], [0, 0, 1], [-1, -self.a1, -self.a2]])
-        self.B = np.array([[0, 0, self.b]])
-        self.C = np.array([1, 0, 0])
+        self.B = np.matrix([[0], [0], [self.b]])
+        self.C = np.matrix([[1], [0], [0]])
 
     # Runs algorithm, plots results
     def runModel(self):
@@ -33,6 +37,7 @@ class DynamicModel:
         plt.xlabel('t - time')
         plt.ylabel('y(t) - output process')
         plt.plot(x, y)
+        
         plt.plot(x, self.y2, c='red')
         plt.plot(x, self.y3, c='green')
         plt.plot(x, self.uk, c='yellow')
@@ -40,28 +45,28 @@ class DynamicModel:
 
     # Algorithm core, loads three y arrays into class
     def generateY(self):
-        phi = np.squeeze(np.array(self.Phi()))
-        gamma = np.squeeze(self.Gamma(self.Phi()))
+        phi = np.matrix(self.Phi())
+        gamma = np.matrix(self.Gamma(self.Phi()))
         l0 = self.l0()
         y = []
         y2 = []
         y3 = []
         uk = []
         num_of_iters = len(self.t_range)
-        x = np.array([])
-        x_prev = np.squeeze(self.xo)
-        uk.append(0)
+        x_prev = self.xo
         y.append(x_prev[0])
         y2.append(x_prev[1])
         y3.append(x_prev[2])
         for k in range(1, num_of_iters):
-            u = np.asscalar(self.U(k-1, l0))
-            x = phi.dot(x_prev) + gamma*u
-            uk.append(u)
-            y.append(x.dot(self.C))
-            y2.append(x[1])
-            y3.append(x[2])
+            u = self.U(k-1, l0)
+            x = np.dot(phi, x_prev) + np.dot(gamma, u)
+            print(x)
+            uk.append(np.asscalar(u))
+            y.append(np.asscalar(x[0]))
+            y2.append(np.asscalar(x[1]))
+            y3.append(np.asscalar(x[2]))
             x_prev = x
+        uk.append(self.U(num_of_iters-1, l0))
         self.uk = uk
         self.y1 = y
         self.y2 = y2
@@ -76,7 +81,7 @@ class DynamicModel:
         phi = np.matrix([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
         q = self.q
         for i in range(q):
-            phi += ((self.A*self.to)**(i+1))/np.math.factorial((i+1))
+            phi += np.linalg.matrix_power(self.A*self.to, i+1)/np.math.factorial((i+1))
         return phi
     
     def Phi_inv(self):
@@ -84,45 +89,48 @@ class DynamicModel:
         q = self.q
         m = -1
         for i in range(q):
-           phi += m*((self.A*self.to)**(i+1))/np.math.factorial((i+1))
+           phi += m*np.linalg.matrix_power(self.A*self.to, i+1)/np.math.factorial((i+1))
            m *= -1
         return phi       
 
     # calculates Gamma to use in main formula, needs Phi
     def Gamma(self, phi):
-        I = np.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-        a =  (phi - I)*self.A**(-1)
-        a1 = np.squeeze(np.array(a))
-        b = np.squeeze(np.array(self.B))
-        return a1.dot(b)
+        return np.dot(np.dot((phi - np.identity(3)), np.linalg.inv(self.A)),self.B)
         
     def U(self, k, l0):
-        return self.G(k).dot(np.transpose(l0))
+        return np.dot(np.transpose(self.G(k)), l0)
 
     def l0(self):
-        Pi = np.linalg.matrix_power(self.Phi(), self.ko-1)
-        Sigma = 0
-        for j in range(0,self.ko):
-            Sigma = Sigma + self.G(j).dot(np.transpose(self.G(j)))
+        
+        Phi = self.Phi()
+        Pi_prev = np.identity(3)
+        for _ in range(self.ko):
+            Pi = np.dot(Phi, Pi_prev)
+            Pi_prev = Pi
+            
 
-        Sigma = np.asscalar(Sigma)
-        L = np.linalg.inv(Pi*Sigma)
-        return L.dot(x_final)       
+        Sigma = np.zeros((3, 3))
+        for j in range(1, self.ko):
+            Sigma = Sigma + np.dot(self.G(j), self.G(j).transpose())
+
+        L = np.linalg.inv(np.dot(Pi, Sigma))
+        return np.dot(L, x_final)      
             
 
     def G(self, j):
         gamma = self.Gamma(self.Phi())
-        P = np.linalg.matrix_power(self.Phi_inv(), j)
-        return P.dot(gamma)
+        phi_inv = self.Phi_inv()
+
+        P_prev = np.identity(3)
+        P = np.identity(3)
+        for _ in range(0,j):
+            P = np.dot(phi_inv, P_prev)
+            P_prev = P
+        return np.dot(P,gamma)
+
 
 
     # for debuging and testing purposes
     def printData(self):
-        print('A ', self.A)
-        print('B ', self.B)
-        print('C ', self.C)
-        print('T ', self.t_range)
-        phi = self.Phi()
-        print('Phi ', phi)
-        print('Gamma ', self.Gamma(phi))
+        pass
 
