e6805b1d2112a44cb15352e0fc49ca328e30a6d8
Daniel Shumeyko <shumeykodanil2@gmail.com>
2019-11-25 18:04:32 +0200

    Added error calculation

--- NumMethods/Lab5/integrals.py
+++ NumMethods/Lab5/integrals.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-# function is ln(x), approx integral on [1;10] is 14.02585
+# function is ln(x), approx integral on [1;10] is 14.02585...
 a = 1
 b = 10
 Eps = 0.01
+M1 = 1
 
 def func(x):
     return np.log(x)
@@ -13,18 +14,20 @@ def left_rectangles():
     I_prev = 0
     steps = 0
     while True:
-        slices = np.arange(a, b + h, h)
+        slices = np.arange(a, b, h)
         I = 0
         for slice in slices:
             I += func(slice)*h
         steps += 1
         if np.absolute(I - I_prev) <= Eps:
-            return I, h, steps
+            err = ((b - a)*M1*h)/2
+            return I, h, steps, err
         else:
             I_prev = I
             h /= 2
 
-I, h, steps = left_rectangles()
+I, h, steps, err = left_rectangles()
 print('I: ' + str(I))
 print('Final h: ' + str(h))
 print('Iterations: ' + str(steps))
+print('Error: ' + str(err))
