30e7ebbf80284e599283f7c1fc6a86cea03c0e70
Daniel Shumeyko <shumeykodanil2@gmail.com>
2020-04-08 21:43:03 +0300

    final

--- StochProcesses/Lab1/main.py
+++ StochProcesses/Lab1/main.py
@@ -1,20 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import math
 
 
 # Create a sample and plot sample path
 x = np.arange(0,20,1)
-y = np.cumsum(np.random.exponential(2,20)) # IMPORTANT Parameter Lambda here is actually 0.5 but we pass parameter beta which = 1/lambda
+y = np.cumsum(np.random.exponential(0.5,20)) # IMPORTANT we pass inverse lambda, so if we want lambda to be 2 we pass 0.5 as the first argument
 
 sns.set()
 plt.step(x,y, c='magenta')
+plt.xlabel('time')
+plt.ylabel('process')
+plt.suptitle('Pisson Process', fontsize=25)
 plt.show()
 
 # Here we calculate the mean value for t = 10
 samples = []
 for _ in range(100):
-    x = np.cumsum(np.random.exponential(2,21))
+    x = np.cumsum(np.random.exponential(0.5,20))
+    print(x[9])
     samples.append(x[9])
 mean = np.mean(samples)
 print('Mean for t=10 is ' + str(mean))
+
